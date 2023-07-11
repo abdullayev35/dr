40ac27094558def743e958d352dfb4f3a91712ec
abdullayev35 <91056227+abdullayev35@users.noreply.github.com>
2023-07-11 14:43:40 +0400

    Add files via upload

---
+++ src/Pages/NewsPage.jsx
@@ -0,0 +1,49 @@
+import React, {useEffect, useState} from 'react';
+import Header from "../components/Header.jsx";
+import Footer from "../components/Footer.jsx";
+import Lang from "../utils/lang.js";
+import {HashLoader} from "react-spinners";
+import News from "../api/news.js";
+import {LazyLoadComponent} from "react-lazy-load-image-component";
+
+const NewsPage = () => {
+
+    const [isFetching, setIsFetching] = useState(true)
+    const [data,setData] = useState([])
+
+    const fetchData = async () =>{
+        const res = await News.getNews()
+        setData(res)
+        setIsFetching(false)
+    }
+
+    useEffect(()=>{
+        fetchData()
+    },[])
+
+    const text = Lang.getCurrentText()
+    return (
+        <div>
+            {isFetching ?
+                <HashLoader style={{position: "absolute", top: "50%", left: "50%"}} color="#4F2CB7" size={70}/>
+                : (
+                    <div className="bariatricPage">
+                        <Header/>
+                        <div className="newsPage-header">{text.news}</div>
+                        <div className="news-container">
+                            {data.map((item) => (
+                                <LazyLoadComponent key={item.id}>
+                                    <SingleBariatric item={item}/>
+                                </LazyLoadComponent>
+                            ))}
+                        </div>
+
+                        <Footer/>
+                    </div>
+                )
+            }
+        </div>
+    )
+};
+
+export default NewsPage;
---
+++ src/components/Bariatric.jsx
@@ -0,0 +1,64 @@
+import React, {useEffect, useState} from 'react';
+import "../assets/style/bariatric.css"
+import Lang from "../utils/lang.js";
+import News from "../api/news.js";
+import {HashLoader} from "react-spinners";
+import {LazyLoadComponent} from 'react-lazy-load-image-component';
+import {
+    Accordion,
+    AccordionItem,
+    AccordionItemHeading,
+    AccordionItemButton,
+    AccordionItemPanel,
+} from 'react-accessible-accordion';
+
+// Demo styles, see 'Styles' section below for some notes on use.
+import 'react-accessible-accordion/dist/fancy-example.css';
+
+const NewsComponent = () => {
+
+    const text = Lang.getCurrentText()
+
+    const [isFetching, setIsFetching] = useState(true)
+    const [data, setData] = useState([])
+    const fetchData = async (count = 6) => {
+        const res = await News.getLastNews({count})
+        setData(res)
+        setIsFetching(false)
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, [isFetching])
+
+    return (
+        <section className="container bariatric-section" id="bariatric">
+            {isFetching ?
+                <HashLoader style={{position: "absolute", top: "50%", left: "50%"}} color="#4F2CB7" size={70}/>
+                : (
+                    <>
+                        <h1>{text.bariatric.toUpperCase()}</h1>
+                        <div className="bariatric-container">
+                            <Accordion className="accordion-container">
+                                {data.map((item) => (
+                                    <AccordionItem className="accordion" key={item.id}>
+                                        <AccordionItemHeading>
+                                            <AccordionItemButton>
+                                                {item?.title}
+                                            </AccordionItemButton>
+                                        </AccordionItemHeading>
+                                        <AccordionItemPanel>
+                                            <div
+                                                dangerouslySetInnerHTML={{__html: item.description}}/>
+                                        </AccordionItemPanel>
+                                    </AccordionItem>
+                                ))}
+                            </Accordion>
+                        </div>
+                    </>
+                )}
+        </section>
+    );
+};
+
+export default NewsComponent;
