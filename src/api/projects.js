9931f112c8102547012a04d90ee61b7a5707f12e
abdullayev35 <91056227+abdullayev35@users.noreply.github.com>
2023-07-11 14:42:39 +0400

    Delete src directory

--- src/api/projects.js
+++
@@ -1,8 +0,0 @@
-import axios from './index.js'
-class Projects {
-    getProjects = params => axios.get('SelectedProjects',{params})
-    getProjectsCount = params => axios.get('SelectedProjects/count',{params})
-    getProject = id => axios.get(`SelectedProjects/${id}`)
-}
-
-export default new Projects()
